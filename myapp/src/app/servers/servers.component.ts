b4f6fc34ba682c98c2e81b91311562dfb4aadfd3
Gopi Srinivasan <igopisrinivasan@gmail.com>
2020-09-29 01:11:29 +0530

    ngStyle

--- myapp/src/app/servers/servers.component.ts
+++ myapp/src/app/servers/servers.component.ts
@@ -10,6 +10,8 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
   serverName = 'TestServer';
+  serverCreated = false;
+
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
@@ -21,6 +23,7 @@ export class ServersComponent implements OnInit {
   onCreateServer() {
     this.serverCreationStatus =
       'Server was created! Name is ' + this.serverName;
+    this.serverCreated = true;
   }
 
   onUpdateServerName(event: Event) {
