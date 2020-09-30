8be71be0146c2a0f1eecc35af5c9adf1cc1cfcb9
Gopi Srinivasan <igopisrinivasan@gmail.com>
2020-09-30 23:10:01 +0530

    Non-Collapsable Navigation Bar

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -5,4 +5,6 @@ import { Component } from '@angular/core';
   selector: 'app-header',
   templateUrl: './header.component.html',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  collapsed = true;
+}
